9257a6369a9f98cb59437f5e6a0771b1c50f9f20
jpujol <josecarlos@loom.com>
2020-07-14 10:27:28 -0700

    Added H264 utils for discovering the presence of a NAL type in a h264 stream (#37)
    * Added H264 utils for discovering the presence of a NAL type in a h264 stream

---
+++ H264Utils.cpp
@@ -0,0 +1,53 @@
+#include "H264Utils.h"
+
+#include <assert.h>
+#include <algorithm>
+
+uint8_t getNalUniTypeValue(H264Utils::NalType nal_type) {
+  switch(nal_type) {
+    case H264Utils::NalType::SliceIdrPicture: return 5;
+    case H264Utils::NalType::SequenceParameterSet: return 7;
+    default: assert(false && "nal unit not encoded");
+  }
+  return 0;
+}
+
+bool H264Utils::hasAnnexBNalOfType(const unsigned char* data, uint32_t length, const std::vector<NalType>& nal_types_to_find) {
+  uint32_t offset = 0;
+
+  if (nal_types_to_find.empty()) {
+    return false;
+  }
+
+  std::vector<uint8_t> nal_types_to_find_value;
+  for (auto nal_type : nal_types_to_find) {
+    nal_types_to_find_value.push_back(getNalUniTypeValue(nal_type));
+  }
+
+  while (offset < length) {
+    bool nal_prefix_found = false;
+    uint32_t nal_prefix_length = 0;
+
+    // 4 byte length NAL prefix: 00, 00, 00, 01
+    if (offset + 4 < length && data[offset] == 0x00 && data[offset + 1] == 0x00 && data[offset + 2] == 0x00 && data[offset + 3] == 0x01 ) {
+      nal_prefix_found = true;
+      nal_prefix_length = 4;
+    } else if (offset + 3 < length && data[offset] == 0x00 && data[offset + 1] == 0x00 && data[offset + 2] == 0x01 ) { // 3 byte length NAL prefix: 00, 00, 01
+      nal_prefix_found = true;
+      nal_prefix_length = 3;
+    }
+
+    if (nal_prefix_found) {
+      offset += nal_prefix_length;
+      uint8_t nal_type = data[offset] & 0x1F;
+      if (std::find(nal_types_to_find_value.begin(), nal_types_to_find_value.end(), nal_type) != nal_types_to_find_value.end()) {
+        return true;
+      }
+    }
+
+    offset++;  // Keep searching
+
+  }
+
+  return false;
+}
---
+++ H264Utils.h
@@ -0,0 +1,20 @@
+#ifndef H264UTILS_H_HEADER
+#define H264UTILS_H_HEADER
+
+#include <stdint.h>
+#include <vector>
+
+namespace H264Utils {
+enum class NalType {
+  SliceIdrPicture,
+  SequenceParameterSet
+};
+
+/**
+ Returns: true, if any of the nal types is found in the stream
+ */
+bool hasAnnexBNalOfType(const unsigned char* data, uint32_t length, const std::vector<NalType>& nal_types_to_find);
+}
+
+#endif  // H264UTILS_H_HEADER
+
